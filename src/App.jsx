bad8d39f8249ff5eacdb44318b66250df93edf4a
Amrutanshu Prusty <amrutanshuprusty4@gmail.com>
2024-11-23 18:36:09 +0530

    Some new changes'

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ function App() {
   const [URL, setURL] = useState("");
 
   const handleURL = (e) => {
-    e.preventDefault();
-    setURL(e.target.value);
-  };
+    e.preventDefault()
+    setURL(e.target.value)
+  }
+
   const downloadSong = async () => {
-    // setURL("")
+    setURL("")
     const options = {
-      method: "GET",
-      url: "https://spotify-downloader9.p.rapidapi.com/downloadSong",
+      method: 'GET',
+      url: 'https://spotify-downloader9.p.rapidapi.com/downloadSong',
       params: {
         songId: `${URL}`,
       },
@@ -25,75 +26,93 @@ function App() {
     };
 
     try {
-      const rspn = await axios.request(options);
+      const rspn = await axios.request(options)
       // console.log(rspn.data.data.downloadLink)
-      window.location.href = rspn.data.data.downloadLink;
+      window.location.href = rspn.data.data.downloadLink
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  downloadSong();
+  // downloadSong();
 
   return (
-    <div className="h-screen w-screen bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-green-600 via-lime-400 to-green-600 flex items-center justify-center flex-col gap-y-4">
-      <div className="flex items-center justify-center gap-x-2 text-xl font-bold">
+    <div className="h-500 w-1000 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-green-600 via-lime-400 to-green-600">
+      <div 
+        className="flex items-center justify-center gap-x-2 text-xl font-bold pt-10">
         <SlSocialSpotify size={50} />
         <p>Song Downloader</p>
       </div>
 
-      <div className="flex gap-x-2">
+      <div className="flex items-center justify-center gap-x-2 m-1">
         <input
           type="url"
-          className="h-10 w-[450px] outline-none px-3 rounded-3xl hover:outline-black"
+          placeholder="Paste URL from Spotify"
+          className="h-10 w-[450px] outline-none px-3 rounded-3xl ring-0 hover:ring-2 hover:ring-black"
           onChange={handleURL} value={URL}
-          />
+        />
         <button
-          className="bg-black text-white h-10 px-3 rounded-3xl font-semibold hover:bg-white hover:text-black hover:font-bold"
+          className="bg-black text-white h-10 px-3 rounded-3xl font-semibold hover:bg-white hover:text-black hover:font-semibold"
           onClick={downloadSong}
           >Download
        </button>
       </div>
+
+      <h1 className="flex items-center justify-center mt-10 text-3xl font-bold">
+        Online & Free Spotify Song Downloader
+      </h1>
+      <p className="flex items-center justify-center mt-5 text-base font-medium  mx-48 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        labore adipisci non fuga facilis minima, magni impedit rerum repudiandae
+        laborum, error quo sint omnis cum maiores iusto nostrum! Facere
+        praesentium facilis deserunt officia laboriosam, cum porro fugit fugiat
+        maxime repudiandae nobis quos incidunt quo nam. Odit mollitia, facilis
+        amet minima doloribus ipsam delectus assumenda veniam, ullam dicta minus
+        asperiores iusto, eveniet maxime. Aliquam, numquam suscipit voluptas
+        veniam cupiditate quidem dolore dolorum magni. Facere odit praesentium
+        aut nisi, ut consequatur perspiciatis vitae modi, in provident neque
+        repellendus! Dolorem molestiae odio, consequatur quaerat quisquam minima
+        laborum ad sunt doloremque reiciendis aperiam id?
+      </p>
+      <div>
+        <h1 className="flex items-center justify-center mt-10 text-3xl font-bold ">
+          How to Download Spotify Song using our downloader
+        </h1>
+
+        <p className="flex items-center justify-center mt-5 text-xl font-bold">
+          Following the below steps👇🏻
+        </p>
+        <div className="flex items-center justify-center flex-col ">
+          <p className="m-5 text-lg font-semibold bg-black text-white px-3 rounded-3xl p-1">
+            1. Play your Spotify song you want to download.
+          </p>
+          <img src="src/1st.PNG" className="h-60" />
+
+          <p className="m-5 text-lg font-semibold  bg-black text-white px-3 rounded-3xl p-1">
+            2. Right click on song -> Click on share -> Copy song link .
+          </p>
+          <img src="src/2nd.PNG" className="h-60" />
+
+          <p className="m-5 text-lg font-semibold  bg-black text-white px-3 rounded-3xl p-1">
+            3. Paste your link in Search Box.
+          </p>
+          <img src="src/3rd.PNG" />
+
+          <p className="m-5 text-lg font-semibold  bg-black text-white px-3 rounded-3xl p-1">
+            4. Click on Download button.
+          </p>
+          <img src="src/4th.PNG" />
+
+          <p className="m-5 text-lg font-semibold  bg-black text-white px-3 rounded-3xl p-1">
+            5. Now you see the download started.
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center bg-black text-white h-20">
+        <p>Copyright ©2024 CodeXintern. </p>
+      </div> 
     </div>
-    // <div className="h-screen w-screen bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-green-700 via-green-700 to-green-500 flex items-center justify-center">
-    //   <div className="flex flex-col text-center justify-center gap-y-4">
-    //     <div className="flex items-center justify-center gap-x-2 text-xl font-bold">
-    //       <SlSocialSpotify size={50} />
-    //       <p>Song Downloader</p>
-    //     </div>
 
-    //     <div>
-    //       <input type="url" className="bg-black text-white rounded-3xl px-2 " />
-    //     </div>
-    //     <div className="flex justify-center">
-    //       <button className="w-24 bg-yellow-200 rounded-xl text-center justify-center  hover:bg-lime-500 hover:text-amber-900 ">
-    //         Download
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
-
-    // <div className="h-screen w-screen bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-green-700 via-green-700 to-green-500 flex items-center justify-center flex-col gap-y-3">
-    //   <div className="flex flex-col text-center justify-center gap-y-4">
-    //     <div className="flex items-center justify-center gap-x-2 text-xl font-bold">
-    //       <SlSocialSpotify size={50} />
-    //       <p>Song Downloader</p>
-    //     </div>
-
-    //     <div className="flex flex-col gap-y-3 text-center justify-center">
-    //       <input
-    //         type="url"
-    //         className="bg-black text-white rounded-3xl px-2 "
-    //         onChange={handleURL}
-    //       />
-    //       <div className="flex justify-center">
-    //         <button className="w-24 bg-yellow-200 rounded-xl text-center justify-center  hover:bg-lime-500 hover:text-amber-900 ">
-    //           Download
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
